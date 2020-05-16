da6b5862f3e9270e4fc967f815e8788eafacdfe9
A-F-V <af695@cam.ac.uk>
2020-05-16 18:33:47 +0100

    Revert "Revert "Changes 2""
    This reverts commit 0ab35f28e0f7116376b8d2eb85d94a92b82c8a70.

--- AFVC/Catalog.cs
+++ AFVC/Catalog.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,25 +10,102 @@ namespace AFVC
 {
     class Catalog
     {
-        private Dictionary<string, string> dict;
+        private CatalogEntry root;
 
         public Catalog(Dictionary<string, string> dict)
         {
-            this.dict = dict;
+            root = new CatalogEntry();
+            GenerateCatalog(dict);
+        }
+
+        private void GenerateCatalog(Dictionary<string, string> dict)
+        {
+            foreach (var key in dict.Keys)
+            {
+                CatalogCode code = new CatalogCode(key);
+                Add(code, dict[key]);
+            }
+        }
+
+        private void Add(CatalogCode code, string s)
+        {
+            CatalogCode parent = code.parent;
+            if(parent.Depth==0)
+                root.Add(code,s);
+            if (Contains(parent))
+            {
+                Get(parent).Add(code, s);
+            }
+            else
+            {
+                Add(parent,"");
+            }
+        }
+
+        private CatalogEntry Get(CatalogCode code)
+        {
+            return root.Get(code);
+        }
+
+        private bool Contains(CatalogCode codePrefix)
+        {
+            return Get(codePrefix) != null;
+        }
+
+        public Catalog()
+        {
+            root = new CatalogEntry();
         }
 
         private class CatalogEntry
         {
-            private CatalogCode codepPrefix;
-        }
+            public CatalogCode codePrefix { get; private set; }
+            public string name { get; private set; }
+            private List<CatalogEntry> children = new List<CatalogEntry>();
 
-        private class CatalogFolder : CatalogEntry
-        {
-            private List<CatalogEntry> children;
-        }
+            public void Add(CatalogCode code, string s)
+            {
+                children.Add(new CatalogEntry(code, s));
+            }
 
-        private class CatalogFile : CatalogEntry { }
+            public CatalogEntry()
+            {
+                codePrefix = null;
+            }
+
+            public CatalogEntry(CatalogCode code, string s)
+            {
+                codePrefix = code;
+                name = s;
+            }
+
+            public CatalogEntry Get(CatalogCode code)
+            {
+                if (code.Equals(CatalogCode.current))
+                {
+                    return this;
+                }
+                else
+                {
+                    CatalogCode rel = CatalogCode.Relative(codePrefix, code);
+                    int ID = rel.CodePattern[0];
+                    CatalogEntry child = GetChild(ID);
+                    return child != null ? child.Get(code) : null;
+                }
+            }
+
+            public CatalogEntry GetChild(int ID)
+            {
+                foreach (var ce in children)
+                {
+                    CatalogCode c = ce.codePrefix;
+                    if (c.CodePattern[c.Depth - 1] == ID)
+                    {
+                        return ce;
+                    }
+                }
+                return null;
+            }
+        }
     }
-
-   
 }
