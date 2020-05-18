dc1f29ab45eab7490abfcc47e65fa71d1159d4d3
Alessandro Farace <af695@cam.ac.uk>
2020-05-18 22:28:21 +0100

    Delete Card

--- AFVC/CatalogManager.cs
+++ AFVC/CatalogManager.cs
@@ -1,45 +1,44 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
-using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Win32;
 using Pastel;
 
 namespace AFVC
 {
-
-    class CatalogManager
+    internal class CatalogManager
     {
-        private readonly int OPTIONS = 6;
         public static readonly string fileLoc = "\\catalog.afv";
         public static readonly string inputFolder = "\\input";
         public static readonly string storage = "\\cards";
         public static readonly string photoviewer = "C:\\Program Files\\Windows Photo Viewer\\PhotoViewer.dll";
-        public static string[] tasks = new string[]{"Update Catalog","View Catalog","Add/Update","Delete","View Card","Close"};
-        private Catalog catalog;
-        private string folder;
-        private Color[] colors = new Color[] {
+
+        public static string[] tasks =
+        {
+            "Update Catalog", "View Catalog", "Add/Update", "Delete Folder", "Delete Card", "View Card(s)",
+            "Close"
+        };
+
+        private static readonly int offdist = 3;
+        private readonly int OPTIONS = tasks.Length;
+        private readonly Catalog catalog;
+
+        private readonly Color[] colors =
+        {
             Color.Crimson,
-            Color.FromArgb(213,254,119),
-            Color.FromArgb(57,240,119),
-            Color.FromArgb(0,201,167),
+            Color.FromArgb(213, 254, 119),
+            Color.FromArgb(57, 240, 119),
+            Color.FromArgb(0, 201, 167),
             Color.MediumPurple,
             Color.DarkMagenta,
             Color.Fuchsia,
             Color.Gold
         };
-        private static int offdist = 3;
 
-        [DllImport("user32.dll")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        private readonly string folder;
 
 
         public CatalogManager(Catalog catalog, string folder)
@@ -50,9 +49,12 @@ namespace AFVC
 
         public CatalogManager(string folder)
         {
-            this.catalog = new Catalog();
+            catalog = new Catalog();
             this.folder = folder;
         }
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
 
 
         public void Run()
@@ -62,53 +64,77 @@ namespace AFVC
             {
                 while (true)
                 {
-                    Console.WriteLine("0 - Update Catalog from Input\n1 - View Catalog\n2 - Add/Update\n3 - Delete\n4 - View Card\n5 - Close");
-                    if(Int32.TryParse(ReadAnswer(), out dec) && dec>=0 && dec<OPTIONS)
+                    Console.WriteLine(
+                        "0 - Update Catalog from Input\n1 - View Catalog\n2 - Add/Update\n3 - Delete Folder\n4 - Delete Card\n5 - View Card(s)\n6 - Close");
+                    if (int.TryParse(ReadAnswer(), out dec) && dec >= 0 && dec < OPTIONS)
                         break;
                 }
 
-                if (dec == 0 || dec == 2 || dec == 3)
+                if (dec == 0 || dec == 2 || dec == 3 || dec == 4 || dec == 6)
                 {
                     Console.WriteLine($"Do you want:\n1 - Back\n2 - {tasks[dec].Pastel(Color.GreenYellow)}");
                     int decC;
-                    Int32.TryParse(ReadAnswer(), out decC);
+                    int.TryParse(ReadAnswer(), out decC);
                     if (decC != 2)
                         continue;
                 }
 
-                switch (dec)
+                try
                 {
-                    case 0:
-                        UpdateCatalogFromInput();
-                        break;
-                    case 1:
-                        Console.WriteLine();
-                        PrintCatalog();
-                        break;
-                    case 2:
-                        AddUpdateRecord();
-                        break;
-                    case 3:
-                        Delete();
-                        break;
-                    case 4:
-                        ViewCard();
-                        break;
+                    switch (dec)
+                    {
+                        case 0:
+                            UpdateCatalogFromInput();
+                            break;
+                        case 1:
+                            Console.WriteLine();
+                            PrintCatalog();
+                            break;
+                        case 2:
+                            AddUpdateRecord();
+                            break;
+                        case 3:
+                            DeleteFolder();
+                            break;
+                        case 4:
+                            DeleteCard();
+                            break;
+                        case 5:
+                            ViewCard();
+                            break;
+                    }
                 }
-            } while (dec != OPTIONS-1);
+                catch (Exception e)
+                {
+                    Console.WriteLine("There was a serious " + "error".Pastel(Color.Red)+ ". Reprompting...");
+                    Thread.Sleep(1000);
+                }
+            } while (dec != OPTIONS - 1);
+        }
+
+        private void DeleteCard()
+        {
+            Console.WriteLine("Insert the code to " + "delete".Pastel(Color.Red));
+            var code = new CatalogCode(ReadAnswer());
+            if (IsCard(code))
+            {
+                var path = Directory.EnumerateFiles(folder + storage + FolderFor(code))
+                    .FirstOrDefault(s => s.Contains(code.ToString()));
+                if (path != default)
+                    File.Delete(path);
+            }
         }
 
         private string TreePrint(CatalogEntry entry, int offset = 0)
         {
-            string thisString = "|-" +
-                                $"{entry.codePrefix.ToString().Pastel(Color.OrangeRed)}. {(entry.name == null ? String.Empty : entry.name.Pastel(colors[Math.Min(entry.codePrefix.Depth - 1, colors.Length - 1)]))} " +
-                                (IsCard(entry.codePrefix) ? " ".PastelBg(Color.Azure) : "")+"\n";
+            var thisString = "|-" +
+                             $"{entry.codePrefix.ToString().Pastel(Color.OrangeRed)}. {(entry.name == null ? string.Empty : entry.name.Pastel(colors[Math.Min(entry.codePrefix.Depth - 1, colors.Length - 1)]))} " +
+                             (IsCard(entry.codePrefix) ? " ".PastelBg(Color.Azure) : "") + "\n";
             foreach (var child in entry.children)
-            {
-                thisString += new String(' ', offset + offdist) + TreePrint(child,offset + offdist);
-            }
+                thisString += new string(' ', offset + offdist) + TreePrint(child, offset + offdist);
             return thisString;
         }
+
         private void PrintCatalog()
         {
             Console.WriteLine(TreePrint(catalog.root));
@@ -117,27 +143,24 @@ namespace AFVC
         private void ViewCard()
         {
             Console.WriteLine("Insert the code to " + "view".Pastel(Color.DeepSkyBlue));
-            CatalogCode code = new CatalogCode(ReadAnswer());
-            if (IsCard(code,out string path))
-            {
+            var code = new CatalogCode(ReadAnswer());
+            if (IsCard(code, out var path))
                 OpenFileProcess(path);
-            }
             else
-            {
                 Console.WriteLine("No card exists.");
-            }
         }
 
         private bool IsCard(CatalogCode code, out string path)
         {
             path = null;
-            string testFolder = folder + storage + FolderFor(code);
+            var testFolder = folder + storage + FolderFor(code);
             if (Directory.Exists(testFolder))
             {
                 path = Directory.EnumerateFiles(testFolder)
                     .FirstOrDefault(p => Path.GetFileNameWithoutExtension(p) == code.ToString());
-                return path != default(string);
+                return path != default;
             }
+
             return false;
         }
 
@@ -146,34 +169,30 @@ namespace AFVC
             return IsCard(code, out var p);
         }
 
-        private void Delete()
+        private void DeleteFolder()
         {
             try
             {
                 Console.WriteLine("Insert the code to " + "delete".Pastel(Color.Red));
-                CatalogCode code = new CatalogCode(ReadAnswer());
+                var code = new CatalogCode(ReadAnswer());
                 catalog.Delete(code);
-                Directory.Delete(folder + storage + FolderFor(code),true);
+                Directory.Delete(folder + storage + FolderFor(code), true);
                 Save();
             }
             catch (Exception e)
             {
-                Console.WriteLine("ERROR".PastelBg(Color.Red)+"IN DELETING");
-                return;
+                Console.WriteLine("ERROR".PastelBg(Color.Red) + "IN DELETING");
             }
         }
 
         private void AddUpdateRecord()
         {
             Console.WriteLine("Insert the new code to add");
-            string input = ReadAnswer();
-            CatalogCode code = new CatalogCode(input);
+            var input = ReadAnswer();
+            var code = new CatalogCode(input);
             Console.WriteLine("Insert the title of this record");
-            string title = ReadAnswer();
-            if (!catalog.Contains(code))
-            {
-                CreateFolderFor(code);
-            }
+            var title = ReadAnswer();
+            if (!catalog.Contains(code)) CreateFolderFor(code);
             catalog.Update(code, title);
             Save();
         }
@@ -187,7 +206,7 @@ namespace AFVC
         {
             if (code.Equals(CatalogCode.current))
                 return "";
-            return "\\" + String.Join("\\", code.CodePattern);
+            return "\\" + string.Join("\\", code.CodePattern);
         }
 
 
@@ -195,27 +214,27 @@ namespace AFVC
         {
             foreach (var pic in Directory.EnumerateFiles(folder + inputFolder))
             {
-
                 var p = PromptOpening(pic);
                 Console.WriteLine("Set the code for this file");
-                CatalogCode code = new CatalogCode(ReadAnswer());
+                var code = new CatalogCode(ReadAnswer());
                 Console.WriteLine("Set title for this file");
-                string title = ReadAnswer();
+                var title = ReadAnswer();
                 CreateFolderFor(code);
-                string path = folder + storage + FolderFor(code) + "\\" + code.ToString() + Path.GetExtension(pic);
-                if(File.Exists(path))
+                var path = folder + storage + FolderFor(code) + "\\" + code + Path.GetExtension(pic);
+                if (File.Exists(path))
                     File.Delete(path);
                 File.Move(pic, path);
-                catalog.Update(code,title);
+                catalog.Update(code, title);
                 p?.Kill();
             }
+
             Save();
         }
 
         private Process PromptOpening(string pic)
         {
             Console.WriteLine($"Would you like to see {Path.GetFileName(pic).Pastel(Color.Aquamarine)}? (Y/N)");
-            string response = Console.ReadLine();
+            var response = Console.ReadLine();
             Process p = null;
             if (response.ToLower() == "y" || response.ToLower() == "yes")
                 p = OpenFileProcess(pic);
@@ -224,9 +243,9 @@ namespace AFVC
 
         private static Process OpenFileProcess(string pic)
         {
-            Process p = Process.Start(pic);
-            Process thisP = Process.GetCurrentProcess();
-            IntPtr s = thisP.MainWindowHandle;
+            var p = Process.Start(pic);
+            var thisP = Process.GetCurrentProcess();
+            var s = thisP.MainWindowHandle;
             SetForegroundWindow(s);
             return p;
         }
@@ -241,16 +260,16 @@ namespace AFVC
 
         private void Save()
         {
-            File.Delete(folder+fileLoc);
+            File.Delete(folder + fileLoc);
             File.Create(folder + fileLoc).Close();
-            File.AppendAllLines(folder+fileLoc,catalog.Serialize());
+            File.AppendAllLines(folder + fileLoc, catalog.Serialize());
         }
 
         private string ReadAnswer()
         {
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Magenta;
-            string output = Console.ReadLine();
+            var output = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
             return output;
