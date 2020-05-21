373e304ea4fb99b4f09743a30830eecf6f4c8967
Alessandro Farace <af695@cam.ac.uk>
2020-05-21 22:23:49 +0100

    Arguments added to program

--- AFVC/Program.cs
+++ AFVC/Program.cs
@@ -14,36 +14,47 @@ namespace AFVC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Files should be named correctly in input folder");
-            Console.WriteLine("Few warnings are given in program. Ensure all codes are correct (01 is incorrect, 1 is correct)");
-            string s;
-            int dec;
-            do
+            CatalogManager m;
+            if (args.Length != 0)
             {
-                Console.WriteLine("0 - New Card System\n1 - Load Card System\n2 - Close");
-                s = Console.ReadLine();
-
-            } while (!Int32.TryParse(s,out dec));
-
-            if (dec == 0 || dec == 1)
-            {
-                var fileContent = string.Empty;
-                var filePath = string.Empty;
-
-                string path = MorePaths.getFolderPath();
-                if(path==null)
-                    return;
-                CatalogManager m;
-                if (dec == 0)
-                {
-                    m = CatalogManager.Setup(path);
-                }
-                else
-                {
-                    m = new CatalogManager(Loader.loaderFromFolder(path),path);
-                }
-                Console.Clear();
+                m = new CatalogManager(Loader.loaderFromFolder(args[0]), args[0]);
                 m.Run();
+            }
+            else
+            {
+                Console.WriteLine("Files should be named correctly in input folder");
+                Console.WriteLine(
+                    "Few warnings are given in program. Ensure all codes are correct (01 is incorrect, 1 is correct)");
+                string s;
+                int dec;
+                do
+                {
+                    Console.WriteLine("0 - New Card System\n1 - Load Card System\n2 - Close");
+                    s = Console.ReadLine();
+
+                } while (!Int32.TryParse(s, out dec));
+
+                if (dec == 0 || dec == 1)
+                {
+                    var fileContent = string.Empty;
+                    var filePath = string.Empty;
+
+                    string path = MorePaths.getFolderPath();
+                    if (path == null)
+                        return;
+
+                    if (dec == 0)
+                    {
+                        m = CatalogManager.Setup(path);
+                    }
+                    else
+                    {
+                        m = new CatalogManager(Loader.loaderFromFolder(path), path);
+                    }
+
+                    Console.Clear();
+                    m.Run();
+                }
             }
         }
     }
